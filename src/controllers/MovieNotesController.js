382f1494f4923346b884ae96a41513054c5330cf
unknown <raulrm.dev@gmail.com>
2023-08-25 18:45:51 -0300

    :tada: first commit

---
+++ src/controllers/MovieNotesController.js
@@ -0,0 +1,104 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+
+class NotesController{
+    async create(request, response){
+        const {title, description, rating, tags} = request.body;
+        const {user_id} = request.params;
+
+        const database = await sqliteConnection();
+        const checkMovieExists = await database.get("SELECT * FROM movieNotes WHERE title = (?) AND user_id = (?)" , [title, user_id])
+
+        if (checkMovieExists){
+            throw new AppError("this movie exists.");
+        }
+
+        if (rating > 5){
+            throw new AppError("the grade cannot be higher than 5.");
+        }
+
+        const [note_id] = await knex("movieNotes").insert({
+            title,
+            description,
+            rating,
+            user_id
+        });
+
+        const tagsInsert = tags.map(name => {
+            return {
+                note_id,
+                name,
+                user_id
+            }
+        });
+
+        await knex("movieTags").insert(tagsInsert);
+
+        response.json();
+    }
+
+    async show(request, response) {
+        const {id} = request.params;
+
+        const note = await knex("movieNotes").where({id}).first();
+        const tags = await knex("movieTags").where({note_id: id}).orderBy("name");
+
+        return response.json({
+            ...note,
+            tags,
+        });
+    }
+
+    async delete(request, response) {
+        const {id} = request.params;
+
+        const database = await sqliteConnection();
+        const checkMovieExists = await database.get("SELECT * FROM movieNotes WHERE id = (?)" , [id]);
+
+        if (!checkMovieExists){
+            throw new AppError("this movie no exists.");
+        }
+
+        await knex("movieNotes").where({ id}).delete();
+
+        return response.json();
+    }
+
+    async index(request, response) {
+        const {user_id, title, tags} = request.query;
+
+        let notes;
+
+        if(tags){
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("movieTags")
+            .select(["movieNotes.id","movieNotes.title","movieNotes.user_id",])
+            .where("movieNotes.user_id", user_id).whereLike("movieNotes.title", `%${title}%`)
+            .whereIn("name",filterTags).innerJoin("movieNotes", "movieNotes.id", "movieTags.note_id")
+            .orderBy("movieNotes.title")
+        }
+
+        else{
+            notes = await knex("movieNotes")
+            .where({user_id})
+            .whereLike("title", `%${title}%`)
+            .orderBy("title");
+        }
+
+        const userTags = await knex("movieTags").where({user_id});
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        });
+
+        return response.json(notesWithTags);
+    }
+}
+
+module.exports = NotesController;
